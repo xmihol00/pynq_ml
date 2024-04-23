8be36a035a36f4781a69a634a8138e88e933c289
David Mihola <david.mihola@sperling.cz>
2024-04-23 23:28:38 +0200

    main-conversion of inputs, weights and predictions to C arrays

--- cnn/cnn_py.py
+++ cnn/cnn_py.py
@@ -1,8 +1,13 @@
+import os
+import sys
 import tensorflow as tf
 from net import get_model, IMAGE_SIZE
 import numpy as np
 from scipy.signal import convolve2d
 from skimage.measure import block_reduce
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from tools import format_array_C
 
 IN_CHANNELS = 3
 L1_OUT_CHANNELS = 4
@@ -13,8 +18,7 @@ L1_WIDTH = IN_WIDTH // 2
 L1_HEIGHT = (IN_HEIGHT - 2) // 2
 L2_WIDTH = L1_WIDTH // 2
 L2_HEIGHT = (L1_HEIGHT - 2) // 2
-L3_WIDTH = L2_WIDTH // 2
-L3_HEIGHT = (L2_HEIGHT - 2) // 2
+L3_OUTPUT_SIZE = 16
 SHIFT = 32
 PRINT = True
 KERNEL_SIZE = 3
@@ -51,9 +55,16 @@ l2_quantized = l2_kernels / l2_max * 127
 l3_quantized = l3_weights / l3_max * 127
 l4_quantized = l4_weights / l4_max * 127
 
-l1_kernels = l1_quantized.reshape(KERNEL_SIZE * KERNEL_SIZE, -1).T
-l2_kernels = l2_quantized.reshape(KERNEL_SIZE * KERNEL_SIZE, -1).T
+l1_kernels = l1_quantized.reshape(KERNEL_SIZE * KERNEL_SIZE, -1).T.astype(np.int32)
+l2_kernels = l2_quantized.reshape(KERNEL_SIZE * KERNEL_SIZE, -1).T.astype(np.int32)
 l3_weights = l3_quantized.astype(np.int32)
+l4_weights = l4_quantized.astype(np.int32)
+
+if PRINT:
+    print("#define KERNEL_WEIGHTS_L1", format_array_C(l1_kernels.reshape(IN_CHANNELS * L1_OUT_CHANNELS, 3, 3)), end="\n\n")
+    print("#define KERNEL_WEIGHTS_L2", format_array_C(l2_kernels.reshape(L1_OUT_CHANNELS * L2_OUT_CHANNELS, 3, 3)), end="\n\n")
+    print("#define MATMUL_WEIGHTS_L3", format_array_C(l3_weights.flatten()), end="\n\n")
+    print("#define MATMUL_WEIGHTS_L4", format_array_C(l4_weights.flatten()), end="\n\n")
 
 kernel_groups_l1 = [[None] * IN_CHANNELS for _ in range(L1_OUT_CHANNELS)]
 channels = []
@@ -82,11 +93,16 @@ for i, group in enumerate(channels):
 l2_kernels = np.array(kernel_groups_l2).astype(np.int32)
 
 correct_predictions = 0
-RANGE = 100
-for i, (sample, expected_class) in zip(range(0, RANGE), train_generator):
+RANGE = 5
+merged_samples = np.zeros((RANGE, IN_CHANNELS * IN_HEIGHT * IN_WIDTH), dtype=np.uint32)
+l3_predictions = np.zeros((RANGE, L3_OUTPUT_SIZE), dtype=np.uint32)
+for n, (sample, expected_class) in zip(range(0, RANGE), train_generator):
     sample = sample.reshape(IN_HEIGHT * IN_WIDTH, -1).T
     sample = sample.reshape(IN_CHANNELS, IN_HEIGHT, IN_WIDTH).astype(np.uint32)
-    # pad sample with zeros on the right and left
+
+    for j in range(IN_CHANNELS):
+        merged_samples[n, j::IN_CHANNELS] = sample[j].flatten()
+
     padded_sample = np.zeros((IN_CHANNELS, IN_HEIGHT, IN_WIDTH + 2)).astype(np.uint32)
     padded_sample[:, :, 1:-1] = sample
     
@@ -113,7 +129,7 @@ for i, (sample, expected_class) in zip(range(0, RANGE), train_generator):
             kernel_sum += convolved
 
         output = np.maximum(kernel_sum, 0)
-        output = (block_reduce(output, (2, 2), np.max) * (1/512)).astype(np.uint32)
+        output = (block_reduce(output, (2, 2), np.max) * (1/256)).astype(np.uint32)
         l2_outputs.append(output[:L2_HEIGHT, :L2_WIDTH])
     
     l2_outputs = np.array(l2_outputs)
@@ -122,10 +138,16 @@ for i, (sample, expected_class) in zip(range(0, RANGE), train_generator):
     
     l3_outputs = (np.dot(l2_outputs, l3_weights) * (1/256))
     l3_outputs = np.maximum(l3_outputs, 0).astype(np.uint32)
+    l3_predictions[n] = l3_outputs.flatten()
+
     l4_outputs = np.dot(l3_outputs, l4_weights)
     predicted_class = l4_outputs[0, 0] >= 0
     expected_class = expected_class[0] >= 0.5
     correct_predictions += predicted_class == expected_class
     print(f"Predicted: {predicted_class},\tExpected: {expected_class}")
+
+if PRINT:
+    print("#define INPUTS", format_array_C(merged_samples), end="\n\n")
+    print("#define PREDICTIONS_L3", format_array_C(l3_predictions), end="\n\n")
 
 print(f"Accuracy: {correct_predictions / RANGE}")
